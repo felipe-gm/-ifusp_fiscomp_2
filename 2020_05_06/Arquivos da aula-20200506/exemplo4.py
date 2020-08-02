057ce43d6268d5113703e50aa97687931bfdd720
felipe-gm <felipe.miyazato@gmail.com>
2020-08-02 18:56:45 -0300

    meaningful folder names and context info added

--- 2020_05_06/Arquivos da aula-20200506/exemplo4.py
+++
@@ -1,6 +0,0 @@
-from random import randrange
-
-print("Vamos gerar um número aleatório.")
-z = randrange(10)
-print("Um segundo número aleatório é",randrange(10))
-print("O primeiro número aleatório foi",z)
--- 2020_05_06/Arquivos da aula-20200506/exemplo5.py
+++
@@ -1,9 +0,0 @@
-from random import randrange,seed
-
-seed(181) # Diferentes argumentos geram diferentes sequências
-print("Vamos gerar um número aleatório.")
-z = randrange(10)
-print("Um segundo número aleatório é",randrange(10))
-print("O primeiro número aleatório foi",z)
-
-
